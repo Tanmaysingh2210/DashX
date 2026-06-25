de763beebcaa59dd6fddfab4bc35179d0520f8c4
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2026-06-25 09:54:42 +0530

    added ping and autosunc

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,8 @@ import passport from "./config/passport.js";// registers the GitHub strategy
 import authRoutes from "./routes/authRoutes.js";
 import activityRoutes from "./routes/activityRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorHandler.js";
+import { startAutoSync } from "./jobs/autoSync.js";
+import cron from "node-cron";
 
 connectDB();
 const app = express();
@@ -15,8 +17,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin:process.env.CLIENT_URL,
-        credentials:true,  // allows cookies to be sent cross-origin
+        origin: process.env.CLIENT_URL,
+        credentials: true,  // allows cookies to be sent cross-origin
     })
 );
 
@@ -25,7 +27,7 @@ app.use(passport.initialize());
 
 
 //health check
-app.get("/", (req,res)=>res.json({status:"DashX API running"}));
+app.get("/", (req, res) => res.json({ status: "DashX API running" }));
 
 app.use("/auth", authRoutes);
 app.use("/activity", activityRoutes);
@@ -33,6 +35,23 @@ app.use("/activity", activityRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+app.get("/ping", (req, res) => {
+  res.send("Server is alive")
+});
+
+cron.schedule("*/5 * * * *", async () => {
+  try {
+    await fetch("https://api.aalsicoders.com/ping");
+    console.log("self ping succesful");
+
+  } catch (error) {
+    console.log("ping failed", error.message);
+  }
+});
+
 
 const port = process.env.PORT;
-app.listen(port , ()=> {`Server running at port ${port}`});
+app.listen(port, () => { `Server running at port ${port}` });
+// start background auto-sync — runs every hour
+// syncs users whose lastSynced > 24h ago
+startAutoSync();
